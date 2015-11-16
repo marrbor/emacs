e885eaa5632576700f2b5c4a815388523d39a0d2
Masahide Matsumoto <matsumoto.masahide@genetec.co.jp>
2015-11-16 13:31:24 +0900

    grep コマンドをデフォルト再帰に変更

--- init.el
+++ init.el
@@ -660,3 +660,23 @@
 (eval-after-load "dired"
   '(define-key dired-mode-map "\C-xt" 'dired-tar))
 
+;; grep
+(define-key global-map (kbd "C-x g") 'grep)
+(require 'grep)
+(setq grep-command "grep -nH -r -e ")
+;(setq grep-command-before-query "grep -nH -r -e ")
+;(defun grep-default-command ()
+;  (if current-prefix-arg
+;      (let ((grep-command-before-target
+;             (concat grep-command-before-query
+;                     (shell-quote-argument (grep-tag-default)))))
+;        (cons (if buffer-file-name
+;                  (concat grep-command-before-target
+;                          " *."
+;                          (file-name-extension buffer-file-name))
+;                (concat grep-command-before-target " ."))
+;              (+ (length grep-command-before-target) 1)))
+;    (car grep-command)))
+;(setq grep-command (cons (concat grep-command-before-query " .")
+;                         (+ (length grep-command-before-query) 1)))
+;
